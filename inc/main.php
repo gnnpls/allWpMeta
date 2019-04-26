7a1f1e4e61272524a4ad428396a927e161fc61d7
gnnpls <n.giannopoulos@motivar.io>
2019-04-26 13:07:16 +0300

    fixes for inptu

--- inc/main.php
+++ inc/main.php
@@ -7,3 +7,19 @@ if (!defined('ABSPATH')) {
 include 'library.php';
 include 'rest_api.php';
 include 'admin/functions.php';
+
+if (!function_exists('awmInputFields')) {
+    function awmInputFields()
+    {
+
+        return apply_filters('awmInputFields_filter', array(
+            'select' => array('label' => __('Select', 'all-wp-meta')),
+            'input' => array('label' => __('Input', 'all-wp-meta')),
+            'date' => array('label' => __('Date', 'all-wp-meta')),
+            'email' => array('label' => __('Email', 'all-wp-meta')),
+            'checkbox' => array('label' => __('Checkbox', 'all-wp-meta')),
+        ));
+
+    }
+
+}
