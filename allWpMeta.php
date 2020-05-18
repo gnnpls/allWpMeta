59499aa7652339f3b63cab9425c2549366891af2
gnnpls <n.giannopoulos@motivar.io>
2020-05-18 19:01:39 +0300

    AllWpMeta update | Automated script

--- allWpMeta.php
+++ allWpMeta.php
@@ -1,4 +1,15 @@
 <?php
+/*
+Plugin Name: all WP Meta
+Plugin URI: https://gnnpls.com
+Description: Add html input meta for admin and frontend using php
+Version: 2
+Author: Giannopoulos Nikolaos
+Author URI: https://gnnpls.com
+Text Domain:       all-wp-meta
+GitHub Plugin URI: https://github.com/Motivar/filox-cloudflare
+GitHub Branch:     master
+ */
 
 if (!defined('WPINC')) {
     die;
